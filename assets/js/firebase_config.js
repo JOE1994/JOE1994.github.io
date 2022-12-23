478016083bc79f216023a845dbe0d86e7da6cae5
JOE1994 <joseph942010@gmail.com>
2022-12-23 06:54:46 -0500

    [firebase] Separate firebase config in own module

---
+++ assets/js/firebase_config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxUl4f1Ly2I3nkYIMGYduP1hwAShCRmm0",
+  authDomain: "workoutdiarydb.firebaseapp.com",
+  projectId: "workoutdiarydb",
+  storageBucket: "workoutdiarydb.appspot.com",
+  messagingSenderId: "860001534872",
+  appId: "1:860001534872:web:5f83f43b611ef47eede1ed",
+  measurementId: "G-XGY5Z9V0W7"
+};
+
+export { firebaseConfig };
--- assets/js/firebase_interact.js
+++ assets/js/firebase_interact.js
@@ -12,17 +12,7 @@ import {
 
 import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBxUl4f1Ly2I3nkYIMGYduP1hwAShCRmm0",
-  authDomain: "workoutdiarydb.firebaseapp.com",
-  projectId: "workoutdiarydb",
-  storageBucket: "workoutdiarydb.appspot.com",
-  messagingSenderId: "860001534872",
-  appId: "1:860001534872:web:5f83f43b611ef47eede1ed",
-  measurementId: "G-XGY5Z9V0W7"
-};
+import {firebaseConfig} from './firebase_config.js'
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
